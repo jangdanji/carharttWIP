c68c7bda903486201b7b2f43b8525f5a01834ca9
jangdanji <defmaybe69@gmail.com>
2023-06-14 14:56:52 +0900

    Updates

---
+++ member.2bb2e51e.js
@@ -0,0 +1,446 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/member.js":[function(require,module,exports) {
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'js/member_data.json');
+xhr.send();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var userInfo = JSON.parse(xhr.responseText);
+
+    /* 로그인 */
+
+    var loginBtn = document.querySelector('.commitBtn');
+    loginBtn.addEventListener('click', function () {
+      var id = document.querySelector('.inputLoginBox input#idBox').value;
+      var pw = document.querySelector('.inputLoginBox input#pwBox').value;
+      if (id == '' || pw == '') return alert('아이디 또는 패스워드를 입력해주세요.');
+      for (var i in userInfo) {
+        if (userInfo[i].id === id && userInfo[i].pw === pw) {
+          alert('환영합니다 ' + id + '님!');
+          window.location.href = './index.html';
+        } else alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+      }
+    });
+
+    /* 아이디 비번 찾기 */
+
+    var _id = document.querySelector('input.idBox');
+    var _tel = document.querySelector('select.telBox');
+    var tel1 = document.querySelector('input#telBox1');
+    var tel2 = document.querySelector('input#telBox2');
+    tel1.addEventListener('input', function () {
+      /* 이건 그냥 첫 4자리 치면 넘어가는거 */
+      if (this.value.length >= 4) tel2.focus();
+    });
+    var btn = document.querySelector('.finder button.commitBtn');
+    btn.addEventListener('click', function () {
+      if (_id.value === '' || tel1.value === '' || tel2.value === '') {
+        return alert('정보를 모두 입력해 주세요.');
+      }
+      for (var i in userInfo) if (_tel.value + tel1.value + tel2.value == userInfo[i].phone && _id.value === userInfo[i].id) {
+        alert('임시 비밀번호가 발송되었습니다.');
+      } else alert('잘못 입력했거나 존재하지 않는 회원입니다.');
+    });
+  }
+};
+
+/* 로그인 창 input 클릭 할 때 밑줄 생기기 */
+
+var inputTextBoxes = document.querySelectorAll('.login label.box input');
+var _loop = function _loop(i) {
+  inputTextBoxes[i].addEventListener('focus', function () {
+    inputTextBoxes[i].parentNode.classList.add('focusOn');
+  });
+  inputTextBoxes[i].addEventListener('blur', function () {
+    inputTextBoxes[i].parentNode.classList.remove('focusOn');
+  });
+};
+for (var i = 0; i < inputTextBoxes.length; i++) {
+  _loop(i);
+}
+
+/* 로고 링크 */
+var home = document.querySelector('.logo');
+home.addEventListener('click', function () {
+  window.location.href = './index.html';
+});
+
+/* 회원가입 검사 */
+
+var id = document.getElementById('idBox');
+var pw = document.getElementById('pwBox');
+var pwChk = document.getElementById('pwChkBox');
+var name = document.getElementById('nameBox');
+var tel = document.querySelectorAll('.telBoxWrap .telBox');
+var addr = document.getElementById('addrBox');
+var signupBtn = document.querySelector('button.commitBtn');
+var allInput = document.querySelectorAll('label.box input');
+id.addEventListener('change', function () {
+  if (id.value.length <= 4) {
+    checking(this, '아이디는 4자리 이상으로 만들어 주세요!', 'red');
+  } else {
+    checking(this, '멋진 아이디네요!', '#545ADC');
+  }
+});
+pw.addEventListener('change', function () {
+  if (pw.value.length <= 8 || pw.value.length >= 16) {
+    checking(this, '8~16자리 비밀번호를 만들어 주세요!', 'red');
+  } else {
+    checking(this, '올바른 비밀번호 입니다.', '#545ADC');
+  }
+});
+pwChk.addEventListener('change', function () {
+  if (pw.value !== pwChk.value) {
+    checking(this, '비밀번호가 일치하지 않아요!', 'red');
+  } else {
+    checking(this, '비밀번호가 일치합니다!', '#545ADC');
+  }
+});
+tel[1].addEventListener('input', function () {
+  if (this.value.length >= 4) tel[2].focus();
+});
+tel[1].addEventListener('change', function () {
+  if (this.value.length <= 3) {
+    checking(this, '전화번호 4자리를 올바르게 입력해주세요!', 'red', this.parentElement.parentElement.querySelector('.textAlert'));
+  } else {
+    checking(this, '', '#545ADC', this.parentElement.parentElement.querySelector('.textAlert'));
+  }
+});
+tel[2].addEventListener('change', function () {
+  if (this.value.length <= 3) {
+    checking(this, '전화번호 4자리를 올바르게 입력해주세요!', 'red', this.parentElement.parentElement.querySelector('.textAlert'));
+  } else {
+    checking(this, '', '#545ADC', this.parentElement.parentElement.querySelector('.textAlert'));
+  }
+});
+signupBtn.addEventListener('click', function () {
+  var alerts = document.querySelectorAll('span.textAlert');
+  for (var _i = 0; _i < alerts.length; _i++) {
+    if (alerts[_i].style.color == 'red') return alert('입력한 정보를 다시 확인해 주세요!');
+  }
+  for (var _i2 = 0; _i2 < allInput.length; _i2++) {
+    if (allInput[_i2].value == '') return alert('정보를 모두 입력해 주세요!!');
+  }
+  if (term1.checked != true || term2.checked != true) return alert('약관에 모두 동의해주세요!');
+  alert('가입되었습니다!');
+  window.location.href = './login.html';
+});
+function checking(element, text, color) {
+  var alertSpan = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : element.previousElementSibling;
+  if (element.value == '') return;
+  alertSpan.textContent = text;
+  alertSpan.style.color = color;
+  alertSpan.style.float = 'right';
+  alertSpan.style.fontSize = '12px';
+  if (color == 'red') element.select();
+}
+
+/* 약관 동의 */
+
+var termAll = document.getElementById('termAll');
+var term1 = document.getElementById('term1');
+var term2 = document.getElementById('term2');
+termAll.addEventListener('change', function () {
+  if (termAll.checked == true) {
+    term1.checked = true;
+    term2.checked = true;
+  } else {
+    term1.checked = false;
+    term2.checked = false;
+  }
+});
+term1.addEventListener('change', function () {
+  if (term1.checked == term2.checked == true) termAll.checked = true;
+});
+term2.addEventListener('change', function () {
+  if (term1.checked == term2.checked == true) termAll.checked = true;
+});
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62640" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/member.js"], null)
+//# sourceMappingURL=/member.2bb2e51e.js.map
